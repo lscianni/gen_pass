494d8515799d59f6e39b93b768b0ba5def1f23a7
lscianni <lscianni@appliedtechres.com>
2018-05-29 18:43:00 -0400

    Add parameter to get multiple passwords

--- gen_pass1.py
+++ gen_pass1.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python
 #
-# Generate a random word from thefreedictionary.com
-# Scrape the word and create a password with the
+# Scrape a word from freedictionary.com and create a password with the
 # minimum complexity requirements
+#
 # Louis Scianni
 # 04/24/2018
 
-import requests
+import requests, random
 from lxml import html
+from sys import argv
 
-
-def request_word():
+def get_args():
+    try:
+        num_words = argv[1]
+    except IndexError:
+        num_words = 1
+    return int(num_words)
+    
+def gen_pass():
+    num = get_args()
     url = 'https://www.thefreedictionary.com/8-letter-words.htm'
     
     sess = requests.Session()
@@ -20,20 +28,13 @@ def request_word():
     tree = html.fromstring(response.content)
     
     words = tree.xpath('//div[@class="TCont"]/ul/li/a/text()')
-    
-    word_lst= []
-    for word in words:
-        word_lst.append(word)
-        
-    scraped_word = words[random.randint(0, len(word_lst))].title() # capitalize word   
-    return scraped_word
-        
-
-
-def gen_passwd(word):
-    sp_char = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '\\', '<', '>', '(', ')', '+', '_', '-', '='] 
-    passwd = '%s%s%d' % (word,sp_char[random.randint(0, len(sp_char))], random.randint(0, 10))
-    print(passwd)
-    
+    #print(len(words))
+    for word in range(0, num):
+        word = words[random.randint(0, len(words)-1)]
+        sp_char = ['!', '@', '#', '$', '%', '^', '&', '*', '/', '\\', '<', '>', '(', ')', '+', '_', '-', '='] 
+        passwd = '%s%s%d' % (word.title(), sp_char[random.randint(0, len(sp_char)-1)], random.randint(0, 10))
+        #passwd = '%s%d' % (word.title(), random.randint(0, 10))
+        print(passwd)
+   
 if __name__ == '__main__':
-    gen_passwd(request_word())
+    gen_pass()
